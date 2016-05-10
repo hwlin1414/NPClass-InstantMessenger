eec0e5e9643c924678926af1197c8c08cc1fc8c1
HWLin <hwlin1414@gmail.com>
2016-05-10 15:18:40 +0800

    modify to prevent sql injection

--- backends/mysql.py
+++ backends/mysql.py
@@ -1,5 +1,6 @@
 import MySQLdb
 import MySQLdb.cursors
+import json
 
 class database(object):
     def __init__(self, host='localhost', port='3306', user=None, passwd=None, db=None):
@@ -28,18 +29,19 @@ class database(object):
             self.c.execute('create table main(`k` varchar(64) not null primary key, `v` varchar(1024) not null);')
 
     def get(self, key, default = None):
-        self.c.execute("select v from main where k = '%s'" % (key))
+        self.c.execute("select v from main where k = %s", (key,))
         res = self.c.fetchall()
         if len(res) == 0: return default
-        return res[0]['v']
+        return json.loads(res[0]['v'])
 
     def set(self, key, val):
-        self.c.execute("select k from main where k = '%s'" % (key))
+        val = json.dumps(val)
+        self.c.execute("select k from main where k = %s", (key,))
         res = self.c.fetchall()
         if len(res) == 0:
-            self.c.execute("insert into main(k, v) value('%s', '%s')" % (key, val))
+            self.c.execute("insert into main(k, v) value(%s, %s)", (key, val))
         else:
-            self.c.execute("update main set v = '%s' where k = '%s'" % (val, key))
+            self.c.execute("update main set v = %s where k = %s", (val, key))
 
     def unset(self, key):
-        self.c.execute("delete from main where k = '%s'" % (key))
+        self.c.execute("delete from main where k = %s", (key,))
