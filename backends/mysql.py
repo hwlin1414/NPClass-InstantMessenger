6ee1d0ea0ab23350fdbb262271b98d7d1968f683
HWLin <hwlin1414@gmail.com>
2016-05-10 15:05:59 +0800

    implement mysql get set unset

--- backends/mysql.py
+++ backends/mysql.py
@@ -18,4 +18,28 @@ class database(object):
         db.autocommit(True)
         self.c = db.cursor()
         self.c.execute('set names \'utf8\'')
+        self.check()
         return self
+
+    def check(self):
+        self.c.execute('show tables')
+        res = self.c.fetchall()
+        if len(res) == 0 or res[0]['Tables_in_imc'] != 'main':
+            self.c.execute('create table main(`k` varchar(64) not null primary key, `v` varchar(1024) not null);')
+
+    def get(self, key, default = None):
+        self.c.execute("select v from main where k = '%s'" % (key))
+        res = self.c.fetchall()
+        if len(res) == 0: return default
+        return res[0]['v']
+
+    def set(self, key, val):
+        self.c.execute("select k from main where k = '%s'" % (key))
+        res = self.c.fetchall()
+        if len(res) == 0:
+            self.c.execute("insert into main(k, v) value('%s', '%s')" % (key, val))
+        else:
+            self.c.execute("update main set v = '%s' where k = '%s'" % (val, key))
+
+    def unset(self, key):
+        self.c.execute("delete from main where k = '%s'" % (key))
